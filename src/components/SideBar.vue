1433afe5da8f6126a6f95d7bea744f4591dcfb7e
CaueVSilv <cauevsilva2002@gmail.com>
2025-03-08 12:50:57 -0300

    ✨ (feat): Add SideBar component
    Created SideBar component and added it to HomeView.

---
+++ src/components/SideBar.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Button from 'primevue/button'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const goRoute = () => {
+  router.push('/')
+}
+</script>
+
+<template>
+  <nav class="sidebar">
+    <Button label="Home" severity="secondary" raised @click="goRoute"></Button>
+  </nav>
+</template>
