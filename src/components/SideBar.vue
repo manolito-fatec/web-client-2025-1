72ebbf418e83ca0012805ed4bf800f20d970f502
CaueVSilv <cauevsilva2002@gmail.com>
2025-03-11 23:19:28 -0300

    💄 (style): Updated SideBar scss
    Fine tuned SideBar scss to have better responsiveness and hover interaction.

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,28 +1,89 @@
 <script setup lang="ts">
-import Button from 'primevue/button'
-import { useRouter } from 'vue-router'
-import SideBarLogo from './SideBarLogo.vue'
+import Button from 'primevue/button';
+import { useRouter } from 'vue-router';
 
-const router = useRouter()
+const router = useRouter();
 
-const goRoute = () => {
-  router.push('/')
-}
+const goDash = () => {
+  router.push('/dashboard');
+};
+const goHome = () => {
+  router.push('/home');
+};
 </script>
 
 <template>
-  <nav class="sidebar">
-    <SideBarLogo class="sidebarLogo"/>
-    <Button label="Home" severity="secondary" raised @click="goRoute"></Button>
-  </nav>
+  <aside>
+    <div class="logo">
+      <img src="../assets/icons/Manolito.png" alt="ManolitoLogo"/>
+      <span class="text">Youtan Dash</span>
+    </div>
+    <div class="sidebar-button-grid">
+      <Button icon="pi pi-objects-column" label="Dashboard" @click="goDash" severity="contrast" variant="text" class="sidebar-routers"></Button>
+      <Button icon="pi pi-home" label="Home" @click="goHome" severity="contrast" variant="text" class="sidebar-routers"></Button>
+      <Button icon="pi pi-user" label="Profile"  @click="goHome" severity="contrast" variant="text" class="sidebar-routers"></Button>
+    </div>
+  </aside>
 </template>
 
-<style lang="css">
-.sidebarLogo{
-  width: 4vw;
-  height: 8vh; /* Always the double of the width */
+<style lang="scss" scoped>
+aside {
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+  background-color: #01081F;
+  width: 300px;
+  height: 100vh;
+  overflow: hidden;
+  left: 0;
+  top: 0;
+  bottom: 0;
+
+  .text {
+    color: #fff;
+    font-size: 1.6rem;
+    font-weight: 700;
+  }
+  .logo{
+    margin-bottom: 1rem;
+    img {
+      width: 4rem;
+    }
+  }
+
+  .sidebar-button-grid {
+  display: grid;
+  position: relative;
+  gap: 0.2rem;
+
+      .sidebar-routers {
+        width: 100%;
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+
+        padding: 0.5rem 1rem;
+        font-size: 1.5rem;
+      
+        &:hover {
+          background-color: #010e38;
+          color: #FFFFFF;
+        }
+    }
+  }
+
+  @media (max-width: 768px) {
+    position: fixed;
+    z-index: 99;
+  }
 }
-/* Barra lateral */
+
+.sidebarLogo {
+  display: block;
+  width: 4vw;
+  height: 8vh; 
+}
+
 .sidebar {
   width: 300px;
   background-color: #01081f;
@@ -36,5 +97,9 @@ const goRoute = () => {
   top: 0;
   bottom: 0;
 }
+
+
+
+
 
 </style>
