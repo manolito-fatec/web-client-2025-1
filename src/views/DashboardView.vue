377e72b57d6e574aae0dd045bc023bd6cb8d254b
CaueVSilv <cauevsilva2002@gmail.com>
2025-03-12 11:53:01 -0300

    ✨ (feat): Added DashboardView component
    Created the component for the main Dashboard View

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+    <div class="dashboard">
+        <h1>Dashboard</h1>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.dashboard{
+    background-color: var(--color-secondary);
+}
+</style>
