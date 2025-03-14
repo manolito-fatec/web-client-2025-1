44624f30d451dcb9b07dc2824561d77d9d3bb642
Beatriz <beatriz.bonatto@fatec.sp.gov.br>
2025-03-14 08:57:08 -0300

    ✨ (feat): Added AverageTimeCard component to DashboardView

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,16 +1,46 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import AvaregeTimeCard from '@/components/AverageTimeCard.vue';
+import axios from 'axios';
 
+const averageTime = ref(0);
+
+// const fetchAverageTime = async () => {
+//     try {
+//         const response = await axios.get('/api/average-time'); // alterar URL
+//         averageTime.value = response.data.averageTimeDay; // Ajustar de acordo com a resposta da API
+//     } catch (error) {
+//         console.error("Erro ao buscar a média", error);
+//     }
+// };
+
+// onMounted(fetchAverageTime);
 
 </script>
 
 <template>
     <div class="dashboard">
         <h1>Dashboard</h1>
+
+        <div class="cards-container">
+            <AvaregeTimeCard titulo="Average Completion Time of Finished Cards" :valor="averageTime"
+                unidade="Cards/week" />
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
-.dashboard{
+.dashboard {
     background-color: var(--color-secondary);
+    color: white;
+    text-align: center;
+    padding: 20px;
+}
+
+.cards-container {
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+    gap: 20px;
 }
 </style>
