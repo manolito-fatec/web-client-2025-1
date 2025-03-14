174591dd2ca2db1d46861ce8efe701551900b860
CaueVSilv <cauevsilva2002@gmail.com>
2025-03-14 17:46:03 -0300

    ✨ (feat): Added default route mapping

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,6 +7,10 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      redirect: '/home',
+    },
+    {
       path: '/dashboard',
       name: 'dashboard',
       component: DashboardView,
