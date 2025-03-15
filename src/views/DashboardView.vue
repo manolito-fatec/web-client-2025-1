7591228e93d9ff07f835394caea08367a04e8a18
CaueVSilv <cauevsilva2002@gmail.com>
2025-03-15 17:12:20 -0300

    💄 (style): Added grid to DashboardView
    Added grid style and multiple TOC to mimic multiple components

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import TotalOfCards from '@/components/TotalOfCards.vue'
+import GraphsByTime from '@/components/GraphsByTime.vue';
 
 </script>
 
@@ -7,7 +8,14 @@ import TotalOfCards from '@/components/TotalOfCards.vue'
     <div class="dashboard">
         <h1>Dashboard</h1>
     </div>
-    <TotalOfCards title="Total de Cards" :total="0"/>
+    <div class="comp-grid">
+        <TotalOfCards title="Total de Cards" :total="0"/>
+        <TotalOfCards title="Total de Cards" :total="0"/>
+        <TotalOfCards title="Total de Cards" :total="0"/>
+        <TotalOfCards title="Total de Cards" :total="0"/>
+        <TotalOfCards title="Total de Cards" :total="0"/>
+        <GraphsByTime></GraphsByTime>
+    </div>
 </template>
 
 <style lang="scss" scoped>
@@ -16,5 +24,19 @@ import TotalOfCards from '@/components/TotalOfCards.vue'
     h1{
         color: #fff;
     }
+}
+.comp-grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr); 
+  gap: 8rem;
+  padding: 1rem;
+
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (max-width: 480px) {
+    grid-template-columns: 1fr; 
+  }
 }
 </style>
