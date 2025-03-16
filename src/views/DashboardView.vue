51acc43604034b641b96650d2a41d62a1f9c5743
CauevSilv <127447047+CauevSilv@users.noreply.github.com>
2025-03-16 14:10:45 -0300

    💄 (style): Added responsiveness triggers
    Added responsiveness triggers to DashBoardView to update the grid component.

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -28,11 +28,24 @@ import GraphsByTime from '@/components/GraphsByTime.vue';
 .comp-grid {
   display: grid;
   grid-template-columns: repeat(3, 1fr); 
-  gap: 8rem;
+  gap: 10%;
   padding: 1rem;
+  max-width: 100%;
+  box-sizing: border-box;
+  overflow-wrap: anywhere;
 
-  @media (max-width: 768px) {
+  @media (max-width: 1286px) {
     grid-template-columns: repeat(2, 1fr);
+
+  }
+  @media (max-width: 1000px) {
+    column-gap: 10px;
+  }
+  @media only screen and (orientation: portrait) and (max-width: 768px) {
+    grid-template-columns: 1fr;
+    row-gap: 20px;
+    padding: 0.5rem;
+  
   }
 
   @media (max-width: 480px) {
