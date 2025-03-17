764f6665159a3f280ca2effd7aab01c87a800a89
Beatriz <beatriz.bonatto@fatec.sp.gov.br>
2025-03-17 19:57:44 -0300

    💄 (style): Added responsive version for mobile

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,23 +1,3 @@
-<script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import AvaregeTimeCard from '@/components/AverageTimeCard.vue';
-import axios from 'axios';
-
-const averageTime = ref(0);
-
-// const fetchAverageTime = async () => {
-//     try {
-//         const response = await axios.get('/api/average-time'); // alterar URL
-//         averageTime.value = response.data.averageTimeDay; // Ajustar de acordo com a resposta da API
-//     } catch (error) {
-//         console.error("Erro ao buscar a média", error);
-//     }
-// };
-
-// onMounted(fetchAverageTime);
-
-</script>
-
 <template>
     <div class="dashboard">
         <h1>Dashboard</h1>
@@ -27,6 +7,13 @@ const averageTime = ref(0);
     </div>
 </template>
 
+<script setup lang="ts">
+import { ref } from 'vue';
+import AvaregeTimeCard from '@/components/AverageTimeCard.vue';
+
+const averageTime = ref(0);
+</script>
+
 <style lang="scss" scoped>
 .dashboard {
     background-color: var(--color-secondary);
@@ -35,10 +22,29 @@ const averageTime = ref(0);
     padding: 20px;
 }
 
+/* Estilo para desktop */
 .cards-container {
     display: flex;
     justify-content: center;
     margin-top: 20px;
     gap: 20px;
+}
+
+@media (max-width: 768px) {
+    .dashboard {
+        padding: 0px;
+    }
+
+    .cards-container {
+        flex-direction: column;
+        align-items: center;
+        gap: 10px; 
+        width: 100%;
+    }
+
+    .cards-container > * {
+        width: 90%;
+        max-width: 400px;
+    }
 }
 </style>
