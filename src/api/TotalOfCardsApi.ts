bb6654bb21e1ed7c94832afd914b8a25f8b9db32
CauevSilv <caue.silva10@fatec.sp.gov.br>
2025-04-09 21:42:08 -0300

    🔧 (chore):updated tasks endpoint url

--- src/api/TotalOfCardsApi.ts
+++ src/api/TotalOfCardsApi.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
-const BASE_URL_TASKS = 'http://localhost:8080/tasks';
+//const BASE_URL_TASKS = 'http://localhost:8080/tasks';
+const BASE_URL_PROD = 'http://http://191.232.189.186:8080/web-server/tasks';
 
 export const fetchTotalOfCards = async (userId:number) => {
     try{
-        const response = await axios.get(BASE_URL_TASKS + '/get-count/' + userId);
+        const response = await axios.get(BASE_URL_PROD + '/get-count/' + userId);
         return response.data;
     }  catch (error){
         throw error
