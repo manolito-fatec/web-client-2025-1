bc34d93b4a5a12c366d3e18976496c70f8949470
CauevSilv <caue.silva10@fatec.sp.gov.br>
2025-04-09 22:09:53 -0300

    🔧 (chore): updated urls to match server

--- src/api/AverageTimeApi.ts
+++ src/api/AverageTimeApi.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
-const BASE_URL_AVERAGE = 'http://localhost:8080/tasks';
+//const BASE_URL_AVERAGE = 'http://localhost:8080/tasks';
+const BASE_URL_PROD = 'http://191.232.189.186:8080/web-server/tasks';
 
 export const fetchAverageTime = async (userId:number) => {
     try {
-        const response = await axios.get(BASE_URL_AVERAGE + '/average-time/' + userId);
+        const response = await axios.get(BASE_URL_PROD + '/average-time/' + userId);
         return response.data;
     } catch (error) {
         console.error("Erro ao buscar dados da API:", error);
--- src/api/GraphQtdByStatusApi.ts
+++ src/api/GraphQtdByStatusApi.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
-const BASE_URL_STATUS = 'http://localhost:8080/status';
+//const BASE_URL_STATUS = 'http://localhost:8080/status';
+const BASE_URL_PROD = 'http://191.232.189.186:8080/web-server/status';
 
 export const fetchTaskStatus = async (userId: number, projectOriginalId: number) => {
     try{
-        const response = await axios.get(BASE_URL_STATUS + '/' + userId + '/' + projectOriginalId);
+        const response = await axios.get(BASE_URL_PROD + '/' + userId + '/' + projectOriginalId);
         return response.data;
     }  catch (error){
         throw error
