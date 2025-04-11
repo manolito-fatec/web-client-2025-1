1279af452187df47268c12f5e3e450ce93793130
Beatriz <beatriz.bonatto@fatec.sp.gov.br>
2025-04-10 23:45:55 -0300

    ✨(feat): add DashboardViewManager component

---
+++ src/views/DashboardViewManager.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+
+import { ref, type Ref } from 'vue';
+
+const currentUserId: Ref<number> = ref<number>(1);
+
+</script>
+
+<template>
+  <div class="dashboard">
+    <h1>Projects Dashboard</h1>
+  </div>
+  <div class="top-grid">
+
+  </div>
+  
+  <div class="bottom-grid">
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.dashboard {
+  background-color: var(--color-secondary);
+
+  h1 {
+    margin: 0%;
+    color: #fff;
+  }
+}
+
+.top-grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 5%;
+  height: 20%;
+  padding: 1rem;
+}
+
+.bottom-grid {
+  display: grid;
+  grid-template-columns: 2fr 1fr; /* 4 ocupa 66%, 5 ocupa 33% */
+  gap: 2%;
+  margin-top: 5%;
+  height: 60%;
+  padding: 0 1rem 1rem 1rem;
+}
+
+/* Responsividade Unificada */
+@media (max-width: 1286px) {
+  .top-grid {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  .bottom-grid {
+    grid-template-columns: 1fr; /* Empilha verticalmente */
+  }
+}
+
+@media only screen and (orientation: portrait) and (max-width: 720px) {
+  h1 {
+    display: none;
+  }
+
+  .top-grid {
+    grid-template-columns: 1fr;
+    gap: 5px;
+    width: 100%;
+    height: 17rem;
+    padding: 0%;
+  }
+  
+  .bottom-grid {
+    grid-template-columns: 1fr;
+    padding: 0%;
+    gap: 5px;
+    height: 30rem;
+  }
+}
+</style>
