f4f3bf74106e6dbe367447920de77be1622ba2a0
CauevSilv <caue.silva10@fatec.sp.gov.br>
2025-04-12 15:47:23 -0300

    🔧 (chore): changed api method name

--- src/api/IssueChartApi.ts
+++ src/api/IssueChartApi.ts
@@ -4,7 +4,7 @@ import { PriorityEnum } from '../enums/PriorityEnum';
 
 const BASE_URL_ISSUE = 'http://localhost:8080/gestor/issue';
 
-export const fetchTaskStatus = async (projectId : number, severity :SeverityEnum , priority: PriorityEnum) => {
+export const fetchIssuesDetailed = async (projectId : number, severity :SeverityEnum , priority: PriorityEnum) => {
     try{
         const response = await axios.get(BASE_URL_ISSUE + '/' + projectId + '/' + severity + '/' + priority);
         return response.data;
