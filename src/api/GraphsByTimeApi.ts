fd1a46b0967a6520a3a386b3667c5ca3afbfca60
CauevSilv <caue.silva10@fatec.sp.gov.br>
2025-04-11 23:35:33 -0300

    🔧 (chore): updated endpoints url

--- src/api/GraphsByTimeApi.ts
+++ src/api/GraphsByTimeApi.ts
@@ -1,12 +1,12 @@
 import type { fetchGraphsBody } from '@/types/GraphsBodyT';
 import axios from 'axios';
 
-//const BASE_URL_STATUS = 'http://localhost:8080/tasks/get-count-between';
-const BASE_URL_PROD = 'http://191.232.189.186:8080/tasks/get-count-between';
+const BASE_URL_STATUS = 'http://localhost:8080/web-server/get-count-between';
+//const BASE_URL_STATUS_LOCAL = 'http://localhost:8080/get-count-between';
 
 export const fetchGraphsByTime = async (paramsJson: fetchGraphsBody) => {
     try{
-        const response = await axios.get(BASE_URL_PROD + '/' +  paramsJson.userIdRef + '/' + paramsJson.startDate + '/' + paramsJson.endDate);
+        const response = await axios.get(BASE_URL_STATUS + '/' +  paramsJson.userIdRef + '/' + paramsJson.startDate + '/' + paramsJson.endDate);
         return response.data;
     }  catch (error){
         throw error
--- src/api/TotalOfCardsApi.ts
+++ src/api/TotalOfCardsApi.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-//const BASE_URL_TASKS = 'http://localhost:8080/tasks';
-const BASE_URL_PROD = 'http://191.232.189.186:8080/tasks';
+const BASE_URL_TASKS = 'http://localhost:8080/web-server/tasks';
+//const BASE_URL_TASKS_LOCAL = 'http://localhost:8080/tasks';
 
 export const fetchTotalOfCards = async (userId:number) => {
     try{
-        const response = await axios.get(BASE_URL_PROD + '/get-count/' + userId);
+        const response = await axios.get(BASE_URL_TASKS + '/get-count/' + userId);
         return response.data;
     }  catch (error){
         throw error
