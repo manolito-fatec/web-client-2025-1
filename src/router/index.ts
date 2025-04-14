916b8fabb8dad174e4f99ca48209346de243c279
Beatriz <beatriz.bonatto@fatec.sp.gov.br>
2025-04-14 20:53:55 -0300

    ✨ (feat): add route for DashboardViewManager in the router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import DashboardView from '../views/DashboardView.vue'
 import ProfileView from '@/views/ProfileView.vue'
+import DashboardViewManager from '@/views/DashboardViewManager.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,6 +14,11 @@ const router = createRouter({
       path: '/dashboard',
       name: 'dashboard',
       component: DashboardView,
+    },
+    {
+      path: '/dashboardManager/',
+      name: 'dashboardManager',
+      component: DashboardViewManager,
     },
     {
       path: '/profile',
