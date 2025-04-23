52af4abc0a30d5b27f39c7405b61c16c5caa3f1c
CauevSilv <127447047+CauevSilv@users.noreply.github.com>
2025-04-22 23:53:12 -0300

    🔧 (chore): updated axios url

--- src/api/TagsByCardApi.ts
+++ src/api/TagsByCardApi.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 import { configHeader } from './AxiosConfig';
 
-const BASE_URL_STATUS = 'http://localhost:8080/status';
+const BASE_URL_STATUS = 'http://localhost:8080/tasks/get-by-tag';
 
 
-export const fetchTagsbyCard = async (userId: number, projectOriginalId: number) => {
+export const fetchTagsbyCard = async (projectOriginalId: number) => {
     try{
-        const response = await axios.get(BASE_URL_STATUS + '/' + userId + '/' + projectOriginalId, configHeader.value);
+        const response = await axios.get(BASE_URL_STATUS + '/' + projectOriginalId, configHeader.value);
         return response.data;
     }  catch (error){
         throw error
