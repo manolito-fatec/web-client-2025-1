d7258e48dbca4d802575f4f0ac2e3441c428dc6e
CauevSilv <127447047+CauevSilv@users.noreply.github.com>
2025-04-25 11:35:45 -0300

    🔀(merge): pull request - feat/R14/id81/cards by period integration (#26)
    * ✨ (feat): cardsByPeriod api function
    
    * 🔥 (chore): remove comments
    
    * ✨ (feat): added chart integration
    
    * ✨ (feat): added cardsByPeriod type
    
    * 📝 (doc): tsdoc for TagsByCards Api
    
    * 📝 (docs): added tsdoc for CardsByPeriod charts
    
    * ✅(test) : added CardsByPeriod vitests
    
    * 🔥 (chore): removed default vitest
    
    * ✅ (test): changed test models
    
    * 🔥 (chore): removed comments
    
    * 🔧 (chore): changed test names

--- src/api/TagsByCardApi.ts
+++ src/api/TagsByCardApi.ts
@@ -3,12 +3,18 @@ import { configHeader } from './AxiosConfig';
 
 const BASE_URL_STATUS = 'http://localhost:8080/tasks/get-by-tag';
 
-
+/**
+ * Fetches tags associated with a project by its project original ID.
+ * @property {number} projectOriginalId - The original ID of the project to fetch tags for.
+ * @returns {Promise<any>} A promise that resolves with the response data containing the tags.
+ * @throws {Error} If the request fails, the error is logged and re-thrown.
+ */
 export const fetchTagsbyCard = async (projectOriginalId: number) => {
     try{
         const response = await axios.get(BASE_URL_STATUS + '/' + projectOriginalId, configHeader.value);
         return response.data;
     }  catch (error){
+        console.log('Erro ao realizar a requisição: ' + error);
         throw error
-    };
+    }
 };
