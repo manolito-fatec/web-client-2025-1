892fdbd25fb92164f0d85173ebcf9a4df6460cad
CauevSilv <127447047+CauevSilv@users.noreply.github.com>
2025-04-24 21:01:23 -0300

    ✨ (feat): added cardsByPeriod type

--- src/api/CardsByPeriodAPI.ts
+++ src/api/CardsByPeriodAPI.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import { configHeader } from './AxiosConfig';
+import type {cardsByPeriodResponse} from "@/types/CardsByPeriodResponse.ts";
 
 const BASE_URL_TASKS = 'http://localhost:8080/tasks/get-project-count-between';
 
 
-export const fetchCardsByPeriod = async (projectOriginalId: number, startDate :string, endDate:string) => {
+export const fetchCardsByPeriod = async (projectOriginalId: number, startDate :string, endDate:string): Promise<cardsByPeriodResponse> => {
     try{
         const response = await axios.get(BASE_URL_TASKS + '/' + projectOriginalId + '/' + startDate + '/' + endDate, configHeader.value);
         return response.data;
