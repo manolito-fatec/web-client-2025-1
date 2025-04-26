d944663ebbcc17375ab749755e1ccb00fe93c038
Beatriz <beatriz.bonatto@fatec.sp.gov.br>
2025-04-25 21:42:40 -0300

    ♻️ (refactor): replace static categories with dynamic project list

--- src/components/StatusCard.vue
+++ src/components/StatusCard.vue
@@ -15,7 +15,10 @@ ChartJS.register(ArcElement, Tooltip, Legend, ChartDataLabels);
 const palette = ['#70728f', '#e47c40', '#e4ce3d', '#94c440', '#56a1e1'];
 const defaultCategories = ['New', 'In Progress', 'Ready for Test', 'Closed', 'Needs Info'];
 
-const categorias = ['New', 'In Progress', 'Ready for Test', 'Closed', 'Needs Info'];
+const projects = [
+  { name: 'Manolito', id: 1637322 },
+  { name: 'Fatec', id: 1657675 }
+];
 
 // Mock data for projects
 const projetos = ref([
