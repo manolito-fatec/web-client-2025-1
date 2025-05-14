2a13c7517e313bef56b6faef320a4d4aec29bdf0
Beatriz <beatriz.bonatto@fatec.sp.gov.br>
2025-05-14 19:51:29 -0300

    ✨ (feat): add TotalCardsByProject component with template and styles

---
+++ src/components/TotalCardsByProject.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="total-card project">
+    <div class="title">Total cards by Project</div>
+
+    <select v-model="SelectProject">
+      project
+    </select>
+
+    <div class="value">0</div>
+  </div>
+</template>
+
+<style scoped>
+.total-card.project {
+  background-color: #01081F;
+  padding: 20px;
+  border-radius: 8px;
+  text-align: center;
+  color: #e0e0e0;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.title {
+  font-size: 1em;
+  color: #3D7EFF;
+  margin-bottom: 10px;
+  font-weight: bold;
+}
+
+select {
+  background-color: #0A1237;
+  color: white;
+  font-size: 0.9rem;
+  padding: 0.02rem;
+  border-radius: 5px;
+  border: none;
+  margin-bottom: 10px;
+  width: 80%;
+  text-align: center;
+}
+
+.value {
+  font-size: 2.5em;
+  font-weight: bold;
+  color: #ffffff;
+  margin-top: 5px;
+}
+</style>
