eae3095cf0b42e8776888e4136171b6f0e359d98
CauevSilv <127447047+CauevSilv@users.noreply.github.com>
2025-05-17 14:54:06 -0300

    🔥 (chore): removed fetchAllUsers api call

--- src/api/GetUsersApi.ts
+++ src/api/GetUsersApi.ts
@@ -1,19 +1,8 @@
 import {api} from './AxiosConfig';
-import type {User} from "@/types/User.ts";
 import type {UserPag} from "@/types/PagUser.ts";
 
-const BASE_URL_USERS = 'http://localhost:8080/user';
 const BASE_URL_USERS_PAG = 'http://localhost:8080/users/paginated';
 
-
-export const fetchAllUsers = async (): Promise<User[]> => {
-    try{
-        const response = await api.get(BASE_URL_USERS + '/' + 'all');
-        return response.data;
-    }  catch (error){
-        throw error
-    }
-};
 
 export const fetchPaginatedUsers = async (): Promise<UserPag[]> => {
     try{
