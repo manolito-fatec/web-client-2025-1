2582446a0acd08cee7b5731803d5550110c30dd2
pauloarantesmahado <pauloantoniomachado@hotmail.com>
2025-05-23 19:37:44 -0300

    :sparkles: (feat): added method to search projects and user list

---
+++ src/types/ProjectUser.ts
@@ -0,0 +1,11 @@
+export type ProjectUser={
+    userId:number;
+    userName:string;
+    projectId:string;
+    projectName:string;
+}
+
+export type ProjectDetails={
+    projectId:string;
+    projectName:string;
+}
