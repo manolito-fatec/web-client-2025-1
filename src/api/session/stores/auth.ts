fce297c20d67a1640e4c872871757e87b81fbb53
CauevSilv <caue.silva10@fatec.sp.gov.br>
2025-06-12 20:08:15 -0300

    🐛 (fix): auth api

--- src/api/session/stores/auth.ts
+++ src/api/session/stores/auth.ts
@@ -14,7 +14,7 @@ export const useAuthStore = defineStore('auth', {
     actions: {
         async loginAndStore(email: string, psw: string) {
             try {
-                const response = await axios.post("http://localhost:8080/auth/login", {
+                const response = await axios.post("http://localhost:3439/auth/login", {
                     email: email,
                     password: psw
                 });
